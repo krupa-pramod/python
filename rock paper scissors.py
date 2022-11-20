2f14c2f282029560907a10164abe7259429c1688
noname <krupa.pramod@gmail.com>
2022-11-20 15:48:29 -0800

    Initial commit

---
+++ rock paper scissors.py
@@ -0,0 +1,32 @@
+import random
+choices = ['rock','paper','scissors']
+print('Rock crushes scissors. Scissors cuts paper. Paper covers rock.')
+player = input('Do you want to be rock, paper, or scissors (or quit)? ')
+while player != 'quit':
+    player = player.lower()
+    computer = random.choice(choices)
+    print ('You choose ' +player+ ', and the computer chose ' +computer+ '.')
+    if player == computer:
+        print("It's a tie!")
+    elif player == 'rock':
+        if computer == "scissors":
+            print ('You win!')
+        else:
+            print('Computer Wins!')
+    elif player == 'paper':
+            if computer == 'rock':
+                print('You win!')
+            else:
+                print('Computer Wins!')
+    elif player =='scissors':
+        if computer == 'paper':
+            print('You win!')
+        else:
+            print('Computer wins!')
+    else: 
+        print('I think there was some sort of error...')
+    print()
+    player = input('Do you want to be rock, paper, or scissors (or quit)? ')
+            
+        
+    
